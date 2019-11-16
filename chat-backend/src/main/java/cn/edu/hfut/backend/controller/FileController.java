86cd0e1e8fb24c851288044a39671500557f4c95
xuewenG <gexuewen@outlook.com>
2019-11-16 20:57:08 +0800

    sassa

--- chat-backend/src/main/java/cn/edu/hfut/backend/controller/FileController.java
+++ chat-backend/src/main/java/cn/edu/hfut/backend/controller/FileController.java
@@ -1,4 +1,36 @@
 package cn.edu.hfut.backend.controller;
 
+import cn.edu.hfut.backend.entity.Response;
+import cn.edu.hfut.backend.util.ResultUtil;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+@RestController
 public class FileController {
+
+    SimpleDateFormat sdf = new SimpleDateFormat("/yyyy/MM/dd/");
+    @PostMapping("/uploadFile")
+    public Response importData(MultipartFile file, HttpServletRequest req, HttpSession httpSession) throws IOException {
+        String format = sdf.format(new Date());
+        String realPath = req.getServletContext().getRealPath("/upload") + format;
+        File folder = new File(realPath);
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
+        String oldName = file.getOriginalFilename();
+        String newName = UUID.randomUUID().toString() + oldName.substring(oldName.lastIndexOf("."));
+        file.transferTo(new File(folder,newName));
+        String url = req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort() + "/upload" + format + newName;
+        System.out.println(url);
+        return ResultUtil.success(url);
+    }
 }
