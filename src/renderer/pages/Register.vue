8b640474423dc96001784a1ab2efba85703fbe72
weixiangchen520 <1981468862@qq.com>
2020-07-05 22:25:55 +0800

    cwx5

--- src/renderer/pages/Register.vue
+++ src/renderer/pages/Register.vue
@@ -169,7 +169,7 @@
         font-family: "Microsoft YaHei";
     }
     .el-aside {
-        height: 680px;
+        height: 100vh;
         background: #f4433c;  /* fallback for old browsers */
         background: -webkit-linear-gradient(to right, #f4433c, #ffab91);  /* Chrome 10-25, Safari 5.1-6 */
         background: linear-gradient(to right, #ffab91, #f4433c); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
